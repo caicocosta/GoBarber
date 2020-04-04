a46e868f0e0fdfe6b3aa96883a5f9b55d0b6738a
Caico <sales.c.costa@gmail.com>
2020-04-04 14:08:51 -0300

    Uploads de imagens utilizando o Multer

--- modulo02/src/routes.js
+++ modulo02/src/routes.js
@@ -3,8 +3,11 @@ import { Router } from 'express';
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
 import authMiddleware from './app/middlewares/auth';
+import multer from 'multer';
+import multerConfig from './config/multer';
 
 const routes = new Router();
+const upload = multer(multerConfig);
 
 routes.post('/user', UserController.store); 
 routes.post('/sessions', SessionController.store); 
@@ -12,6 +15,9 @@ routes.post('/sessions', SessionController.store);
 routes.use(authMiddleware);
 
 routes.put('/user', UserController.update); 
+routes.post('/files', upload.single('file'), (req, res) =>{
+  return res.json({ ok: true });
+})
 
 
 
