c569a6c3284261fa8a5ff21a2eda9b085bd8ff6f
Caico <sales.c.costa@gmail.com>
2020-04-06 15:51:53 -0300

    Criado a função de agendamento de atendimentos. Criado a busca dos agendamentos realizados por usuário(Com paginação configurada;)

--- modulo02/src/routes.js
+++ modulo02/src/routes.js
@@ -8,6 +8,7 @@ import multer from 'multer';
 import multerConfig from './config/multer';
 import fileController from './app/controllers/FileController';
 import userController from './app/controllers/ProviderController';
+import AppointmentController from './app/controllers/AppointmentController';
 
 const routes = new Router();
 const upload = multer(multerConfig);
@@ -20,6 +21,9 @@ routes.use(authMiddleware);
 routes.put('/user', UserController.update); 
 
 routes.get('/providers', userController.index);
+
+routes.get('/appointments', AppointmentController.index);
+routes.post('/appointments', AppointmentController.store);
 
 routes.post('/files', upload.single('file'), fileController.store);
 
