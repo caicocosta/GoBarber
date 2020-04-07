0c2e4532543c363c1dcfec0c5e61fe445a90c749
Caico <sales.c.costa@gmail.com>
2020-04-07 13:24:13 -0300

    Finalizando a parte de agendamentos. Inclusão da função para notificação para os providers sobre novos agendamentos. Inclusão da função de update de angendamentos, sejam elas marcadas como lidas, ou canceladas.

--- modulo02/src/routes.js
+++ modulo02/src/routes.js
@@ -10,6 +10,7 @@ import fileController from './app/controllers/FileController';
 import userController from './app/controllers/ProviderController';
 import AppointmentController from './app/controllers/AppointmentController';
 import ScheduleController from './app/controllers/ScheduleController';
+import NotificationController from './app/controllers/NotificationController';
 
 const routes = new Router();
 const upload = multer(multerConfig);
@@ -25,8 +26,12 @@ routes.get('/providers', userController.index);
 
 routes.get('/appointments', AppointmentController.index);
 routes.post('/appointments', AppointmentController.store);
+routes.delete('/appointments/:id', AppointmentController.delete);
 
 routes.get('/schedule', ScheduleController.index);
+
+routes.get('/notifications', NotificationController.index);
+routes.put('/notifications/:id', NotificationController.update);
 
 routes.post('/files', upload.single('file'), fileController.store);
 
