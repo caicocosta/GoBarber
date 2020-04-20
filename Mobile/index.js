4ed390fce3ed4f38ae93ed2b296d61217dde3945
Caico <sales.c.costa@gmail.com>
2020-04-20 18:02:07 -0300

    Inicio do Front-End com a versão Mobile do goBarber.

---
+++ Mobile/index.js
@@ -0,0 +1,9 @@
+/**
+ * @format
+ */
+
+import {AppRegistry} from 'react-native';
+import App from './src';
+import {name as goBarber} from './app.json';
+
+AppRegistry.registerComponent(goBarber, () => App);
